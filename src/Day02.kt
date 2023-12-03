f93d86af3d8add97a6266669214af672db62b518
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-12-03 15:56:36 -0300

    Puzzle 2 solution, idiomatic enhancement.

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,35 +3,40 @@ fun main() {
     fun splitInput(input: String): List<String> = input.split(": ", ", ", "; ", " ")
 
     fun part1(input: List<String>): Int {
-        val limits = mapOf("red" to 12, "green" to 13,"blue" to 14)
-        return input.map input@{
-            val split = splitInput(it)
+        val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+        fun gameViability(input: String): Pair<Int, Boolean> {
+            val split = splitInput(input)
             val gameNumber = split[1].toInt()
             for (index in 2..<split.size step 2) {
                 val color = split[index + 1]
                 val value = split[index].toInt()
                 if (value > limits[color] ?: Int.MAX_VALUE) {
-                    return@input gameNumber to false
-
+                    return gameNumber to false
                 }
             }
-            gameNumber to true
+            return gameNumber to true
         }
-        .filter { (_, isGamePossible) -> isGamePossible }
-        .sumOf { (gameNumber, _) -> gameNumber }
+
+        return input.map(::gameViability)
+            .filter { (_, isGamePossible) -> isGamePossible }
+            .sumOf { (gameNumber, _) -> gameNumber }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map input@{
+
+        fun minimumCubesPower(input: String): Int {
             val maxQuantityGrabbed = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
-            val split = splitInput(it)
+            val split = splitInput(input)
             for (index in 2..<split.size step 2) {
                 val color = split[index + 1]
                 val value = split[index].toInt()
                 maxQuantityGrabbed.merge(color, value, ::maxOf)
             }
-            maxQuantityGrabbed.values.reduce(Int::times)
-        }.sum()
+            return maxQuantityGrabbed.values.reduce(Int::times)
+        }
+
+        return input.map(::minimumCubesPower).sum()
     }
 
     // test if implementation meets criteria from the description:
