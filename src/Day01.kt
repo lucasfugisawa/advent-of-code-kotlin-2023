a9642208d03227ec5d12dad7ed34413948360067
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-12-03 15:33:02 -0300

    Puzzle 1 solution, idiomatic enhancement.

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,7 +13,7 @@ fun main() {
 
     fun part2(input: List<String>): Int {
 
-        fun String.findFirstSubstringAmong(words: Set<String>): String? {
+        infix fun String.firstSubstringAmong(words: Set<String>): String? {
             var firstSubstringIndex = Int.MAX_VALUE
             var firstSubstring: String? = null
             for (word in words) {
@@ -27,17 +27,14 @@ fun main() {
         }
 
         fun calibrationValue(word: String): Int {
-
             val digitsMap = mapOf(
                 "0" to 0, "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9,
                 "zero" to 0, "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
             )
             val reversedDigitsMap = digitsMap.map { (key, value) -> key.reversed() to value }.toMap()
-
-            val reversedWord = word.reversed();
-
-            val first = digitsMap[word.findFirstSubstringAmong(digitsMap.keys) ?: "0"] ?: 0
-            val last = reversedDigitsMap[reversedWord.findFirstSubstringAmong(reversedDigitsMap.keys) ?: "0"] ?: 0
+            val reversedWord = word.reversed()
+            val first = digitsMap[word firstSubstringAmong digitsMap.keys] ?: 0
+            val last = reversedDigitsMap[reversedWord firstSubstringAmong reversedDigitsMap.keys] ?: 0
             return (first * 10 + last)
         }
         return input.sumOf(::calibrationValue)
