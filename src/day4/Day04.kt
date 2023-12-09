3b29696bf1386bed7adc338841a6be6f5b66a70b
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-12-09 18:13:14 -0300

    Day 4, part 2 solution.

--- src/day4/Day04.kt
+++ src/day4/Day04.kt
@@ -6,33 +6,61 @@ import kotlin.math.pow
 
 fun main() {
 
-    fun part1(lines: List<String>): Int = lines.map(String::toCard).sumOf(Card::points)
+    fun part1(deck: Deck): Int = deck.points
 
-    fun part2(lines: List<String>): Int = 30 // TODO: Implement.
+    fun part2(deck: Deck): Int = deck.processedTotal
 
     // test if implementation meets criteria from the description:
     val testInput = readInput("day4/puzzleTestData.txt")
-    check(part1(testInput) == 13)
-    check(part2(testInput) == 30)
+    val testDeck = Deck(testInput.map(String::toCard))
+    check(part1(testDeck) == 13)
+    check(part2(testDeck) == 30)
 
     // calculate puzzle data solution:
     val input = readInput("day4/puzzleData.txt")
-    part1(input).println()
-    part2(input).println()
+    val deck = Deck(input.map(String::toCard))
+    part1(deck).println()
+    part2(deck).println()
 }
 
 class Card(
-    val number: Int,
+    private val number: Int,
     val winningNumbers: List<Int>,
     val availableNumbers: List<Int>,
 ) {
     val points: Int by lazy { availableNumbers.count { it in winningNumbers }.let(::calculatePoints) }
 
-    private fun calculatePoints(numberOfWinningNumbers: Int): Int =
-        if (numberOfWinningNumbers == 0) 0 else 2.0.pow(numberOfWinningNumbers - 1).toInt()
+    private fun calculatePoints(matchesCount: Int): Int =
+        if (matchesCount == 0) 0 else 2.0.pow(matchesCount - 1).toInt()
 
-    override fun toString(): String =
-        "Card $number: ${winningNumbers.joinToString(" ")} | ${availableNumbers.joinToString(" ")} -> $points"
+    override fun toString(): String {
+        fun leftPadWithSpace(number: Int) = number.toString().padStart(2, ' ')
+        return "Card $number: " +
+                "${winningNumbers.joinToString(" ", transform = ::leftPadWithSpace)} | " +
+                "${availableNumbers.joinToString(" ", transform = ::leftPadWithSpace)} -> $points"
+    }
+}
+
+class Deck(
+    private val cards: List<Card>,
+) {
+    val points: Int by lazy { cards.sumOf(Card::points) }
+    val processedTotal: Int by lazy { calculateTotalScratchcards() }
+
+    private fun calculateTotalScratchcards(): Int {
+        val counter = IntArray(cards.size) { 1 }
+
+        cards.forEachIndexed { i, card ->
+            val score = card.availableNumbers.count { it in card.winningNumbers }
+            for (j in (i + 1)..minOf(i + score, cards.size - 1)) {
+                counter[j] += counter[i]
+            }
+        }
+
+        return counter.sum()
+    }
+
+    override fun toString(): String = cards.joinToString("\n")
 }
 
 private val cardRegex = """Card\s+(\d+):\s+(\d+(?:\s+\d+)*)\s*\|\s*(\d+(?:\s+\d+)*)""".toRegex()
